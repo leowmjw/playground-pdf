5a5e63b1bff7c418c1f2e53f53052e6e5b6b5b84
Michael Leow <leow@sinarproject.org>
2015-10-14 18:24:29 +0800

    Summary: Add initial EC Parser engine

---
+++ ECRedelineation/src/org/sinarproject/ECRedelineation.java
@@ -0,0 +1,119 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.sinarproject;
+
+import com.itextpdf.text.pdf.PdfReader;
+import com.itextpdf.text.pdf.parser.GlyphTextRenderListener;
+import com.itextpdf.text.pdf.parser.LocationTextExtractionStrategy;
+import com.itextpdf.text.pdf.parser.PdfTextExtractor;
+import com.itextpdf.text.pdf.parser.TextExtractionStrategy;
+import java.io.IOException;
+import static java.lang.System.out;
+import java.util.Map;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import static org.boon.Maps.map;
+import org.boon.primitive.Int;
+
+/**
+ *
+ * @author leow
+ */
+public class ECRedelineation {
+
+    public static String SOURCE_2ND = "./example/Sarawak_2ndSchedule.pdf";
+    public static String SOURCE = "./example/Sarawak_Proposal.pdf";
+    static PdfReader my_reader;
+    static int currentScheduleBlock = 0;
+    static String currentPARLabel;
+    static String currentDUNLabel;
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        out.println("Sinar Project's EC Parser ..");
+        PdfReader reader = null;
+        try {
+            reader = new PdfReader(SOURCE);
+            int n;
+            n = reader.getNumberOfPages();
+            int i;
+            for (i = 1; i < n; i++) {
+                try {
+                    String content;
+                    
+                    content = PdfTextExtractor.getTextFromPage(reader, i, new LocationTextExtractionStrategy());
+                    describePage(content, i);
+                } catch (IOException ex) {
+                    Logger.getLogger(ECRedelineation.class.getName()).log(Level.SEVERE, null, ex);
+                }
+            }
+        } catch (IOException ex) {
+            Logger.getLogger(ECRedelineation.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+
+    private static void describePage(String raw_extracted_content, int current_page) {
+        // DEBUG: 
+        // out.print(raw_extracted_content);
+        if (Utils.isStartOfSchedule(raw_extracted_content)) {
+            out.println("Mark page " + current_page + " as a NEW Block");
+        };
+        switch (currentScheduleBlock) {
+            case 1:
+                // DO nothing .. for now ..
+                break;
+            case 2:
+                /*
+                 Map<String, Map<String, Map<String, Int>>> m;
+                 m = null;
+                 Map<Character, Map<Character, Map<Character, Integer>>> n;
+                 n = map('d',
+                 map('c',
+                 map('s', 1)
+                 )
+                 );
+                 */
+                // Esier code 123/12/33
+                //  returns a snippet instead ..
+                // Arrays of DMs, DMs[] = ""
+                // Init PAR structure??
+                // [PAR][DUN][DM]
+                // Map<String, Map<String,Map<String,Int>>>
+                // Do something ..
+                // Look for PAR Header; extract out the value ..
+                // Break apart the content by line ..
+                // match ..
+                String[] lines_of_content = raw_extracted_content.split("\\r?\\n");
+                for (String single_line_of_content : lines_of_content) {
+                    // DEBUG:
+                    out.println("LINE:" + single_line_of_content);
+                    if (Utils.isStartOfPAR(single_line_of_content)) {
+                        // Anything to do with PAR??
+                        out.println(single_line_of_content);
+                        out.println("========^^^ MATCHED PAR ^^^^=======");
+                    } else if (Utils.isStartOfDUN(single_line_of_content)) {
+                        // ANything to do DUN
+                        out.println(single_line_of_content);
+                        out.println("========^^^ MATCHED DUN ^^^^=======");
+                    } else if (Utils.containsDMData(single_line_of_content)) {
+                        // Extract DM
+                    } else {
+                        // Nothing to do ..
+                    };
+                }
+                break;
+            case 3:
+                // Do nothing ... for now ..
+                break;
+            default:
+                break;
+        }
+    }
+
+}
---
+++ ECRedelineation/src/org/sinarproject/Utils.java
@@ -0,0 +1,112 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.sinarproject;
+
+import static java.lang.System.out;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+/**
+ *
+ * @author leow
+ */
+public class Utils {
+
+    // Patterns for the various regexp used
+    // No need to use Pattern/Matcher style; just direct with regexp adjustable here ..
+    private static final String more_than_one_space_regexp = "\\s+";
+    private static final String first_schedule = "JADUAL PERTAMA";
+    private static final Pattern first_schedule_pattern = Pattern.compile(first_schedule);
+    private static final String second_schedule = "JADUAL KEDUA";
+    private static final Pattern second_schedule_pattern = Pattern.compile(second_schedule);
+    private static final String third_schedule = "JADUAL KETIGA";
+    private static final Pattern third_schedule_pattern = Pattern.compile(third_schedule);
+    private static final String PAR_regexp = "PERSEKUTUAN.*?P.*?(\\d+).*?([\\w\\s]+).*?";
+    private static final Pattern PAR_regexp_pattern = Pattern.compile(PAR_regexp);
+    private static final String DUN_regexp = "N\\s*?\\.\\s*?(\\d+).*?([\\w’'\\s]+?)\\s*?(\\d+\\..*)";
+    private static final Pattern DUN_regexp_pattern = Pattern.compile(DUN_regexp);
+    private static final String DUN_regexp_loose = "N\\.";
+    private static final String DM_regexp = "(\\d+?)\\s*?\\.\\s*?([\\w’'\\s]+?)\\s*?(\\d[,\\d]+).*?";
+    private static final Pattern DM_regexp_pattern = Pattern.compile(DM_regexp);
+    private static final String DM_regexp_loose = "";
+    private static final String population_total_regexp = "\\s+\\d+";
+
+    public static boolean isStartOfSchedule(String raw_content) {
+
+        if (first_schedule_pattern.matcher(raw_content).find()) {
+            out.println("Found FIRST SCHEDULE!!" + raw_content.matches(first_schedule));
+            ECRedelineation.currentScheduleBlock = 1;
+            return true;
+        }
+        if (second_schedule_pattern.matcher(raw_content).find()) {
+            out.println("Found SECOND SCHEDULE!!");
+            ECRedelineation.currentScheduleBlock = 2;
+            return true;
+        }
+        if (third_schedule_pattern.matcher(raw_content).find()) {
+            out.println("Found THIRD SCHEDULE!!");
+            ECRedelineation.currentScheduleBlock = 3;
+            return true;
+        }
+        // Nothing matched; is just an ordinary page ..
+        return false;
+    }
+
+    public static boolean isStartOfPAR(String single_line_of_content) {
+        Matcher PAR_regexp_pattern_matched = PAR_regexp_pattern.matcher(single_line_of_content);
+        if (PAR_regexp_pattern_matched.find()) {
+            out.println("CODE:" + PAR_regexp_pattern_matched.group(1)
+                    + " NAME:" + PAR_regexp_pattern_matched.group(2));
+            return true;
+        }
+        // Extract out PAR Code 
+        // Update the current PAR Code label
+        return false;
+
+    }
+
+    public static boolean isStartOfDUN(String single_line_of_content) {
+        Matcher DUN_regexp_pattern_matched = DUN_regexp_pattern.matcher(single_line_of_content);
+        if (DUN_regexp_pattern_matched.find()) {
+            out.println("CODE:" + DUN_regexp_pattern_matched.group(1)
+                    + " NAME:" + DUN_regexp_pattern_matched.group(2)
+                    + " LEFTOVER:" + DUN_regexp_pattern_matched.group(3));
+
+            extractDataOfDM(DUN_regexp_pattern_matched.group(3));
+            return true;
+        }
+        // Need for a more flexible match as well ..
+        // Extract out DUN Code and also first line of DM for downstream processing
+        // Update the current DUN Code label
+        return false;
+
+    }
+
+    public static boolean containsDMData(String single_line_of_content) {
+        // Use a strict focus ..
+        // Need for a more flexible match as well ..
+        return false;
+
+    }
+
+    public static boolean extractDataOfDM(String single_line_of_content) {
+        Matcher DM_regexp_pattern_matched = DM_regexp_pattern.matcher(single_line_of_content);
+        if (DM_regexp_pattern_matched.find()) {
+            out.println("CODE:" + DM_regexp_pattern_matched.group(1)
+                    + " NAME:" + DM_regexp_pattern_matched.group(2)
+                    + " POPULATION:" + DM_regexp_pattern_matched.group(3));
+            return true;
+        }
+        // Detect and attach to last found DUN ..
+        return false;
+
+    }
+
+    private static void normalizeCode(String raw_code_number) {
+        // Fixed to double digit ..
+    }
+
+}
